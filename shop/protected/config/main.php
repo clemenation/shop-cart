1533c36f6b87c7954459aed1121251055b0ea770
Dung Nguyen <clemenation@gmail.com>
2013-05-08 23:02:29 +0700

    App name

--- shop/protected/config/main.php
+++ shop/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Yii Shop',
+	'name'=>'Laptop Buy',
     'language' => 'vi',
 	// preloading 'log' component
 	'preload'=>array('log'),
